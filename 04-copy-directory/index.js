3d0b7cb70a8ae830fa82b172d2524b26136e6103
vladklen <Vlad6097944@gmail.com>
2023-05-09 11:43:22 +0300

    fix:task4  remove files fixed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,20 +6,35 @@ const newPath = path.join(__dirname, 'files-copy');
 const oldPath = path.join(__dirname, 'files');
 
 const copyDir = () => {
-  fs.promises.mkdir(newPath, { recursive: true }, function (err) {
+  fsPromises.mkdir(newPath, { recursive: true }, function (err) {
     if (err) console.log(err);
   });
 
-  fs.readdir(oldPath, (err, data) => {
-    data.forEach((file) => {
-      if (file.split('.')[1] && file.split('.')[1].length > 0) {
-        fs.copyFile(`${oldPath}/${file}`, `${newPath}/${file}`, (err) => {
-          if (err) throw err;
-          console.log(`file was copied`);
+  fsPromises
+    .readdir(newPath)
+    .then((data, err) => {
+      data.forEach((file) => {
+        if (file.split('.')[1] && file.split('.')[1].length > 0) {
+          console.log(1);
+          fs.unlink(`${newPath}/${file}`, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    })
+    .then(() => {
+      fsPromises.readdir(oldPath).then((data, err) => {
+        data.forEach((file) => {
+          if (file.split('.')[1] && file.split('.')[1].length > 0) {
+            console.log(2);
+            fs.copyFile(`${oldPath}/${file}`, `${newPath}/${file}`, (err) => {
+              if (err) throw err;
+              console.log(`file was copied`);
+            });
+          }
         });
-      }
+      });
     });
-  });
 };
 
 copyDir();
